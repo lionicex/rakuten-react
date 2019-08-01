f6f68bd77accee088c22d86ab406b4a06977a740
David Gonzalez Ramos <lionicex@gmail.com>
2019-08-01 17:55:23 +0200

    fix responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,57 +3,99 @@ import '../css/footer.css';
 
 const Footer = () => (
 
-    <div className="footer">
-        <div className="footer__info">
-            <h1 className="footer__info__title">Rakuten TV - Tu cine en casa</h1>
-            <p className="footer__info__text">
-                Disfruta de los últimos estrenos en tu SMART TV y muchos otros
-                dispositivos en tan
-                sólo unos clics. Los mejores blockbusters, clásicos premiados y series al instante. Sin contratos ni
-                compromisos. Pruébalo ahora y gana Rakuten Súper Puntos con cada compra que realices.
-            </p>
-            <a className="footer__info__block footer__info__block--howitworks" href="https://ayuda.rakuten.tv"
-               target="_blank">
-                <span className="footer__info__block__title">Cómo funciona</span>
-                <span className="footer__info__block__text">Rakuten TV es compatible con la gran mayoría de dispositivos conectados.</span>
-            </a>
-            <a className="footer__info__block footer__info__block--payments" href="https://ayuda.rakuten.tv"
-               target="_blank">
-                <span className="footer__info__block__title">Más formas de pago</span>
-                <span className="footer__info__block__text">Rakuten TV es compatible con PayPal y con tarjetas de crédito Visa y Mastercard.</span>
-            </a>
-            <a className="footer__info__block footer__info__block--support" href="https://ayuda.rakuten.tv"
-               target="_blank">
-                <span className="footer__info__block__title">Ayuda y soporte</span>
-                <span className="footer__info__block__text">¿Tienes problemas con nuestro servicio? Ponte en contacto con nuestro equipo de soporte</span>
-            </a>
+    <div id="footer">
+        <div className="text-section">
+            <div className="first-text">
+                <h2>Rakuten TV - Tu cine en casa</h2>
+                <p>
+                    Disfruta de los últimos estrenos en tu SMART TV y muchos otros dispositivos en tan sólo unos clics.
+                    Los mejores blockbusters, clásicos premiados y series al instante. Sin contratos ni compromisos.
+                    Pruébalo ahora y gana Rakuten Súper Puntos con cada compra que realices.
+                </p>
+            </div>
+            <div className="second-text">
+                <a className="devices" href="https://ayuda.rakuten.tv">
+                    <h2>Cómo funciona</h2>
+                    <p>Rakuten TV es compatible con la gran mayoría de dispositivos conectados.</p>
+                </a>
+                <a className="payment" href="https://ayuda.rakuten.tv">
+                    <h2>Más formas de pago</h2>
+                    <p>Rakuten TV es compatible con PayPal y con tarjetas de crédito Visa y Mastercard.</p>
+                </a>
+                <a className="help" href="https://ayuda.rakuten.tv">
+                    <h2>Ayuda y soporte</h2>
+                    <p>¿Tienes problemas con nuestro servicio? Ponte en contacto con nuestro equipo de soporte</p>
+                </a>
+            </div>
         </div>
-        <div className="footer__mini">
-            <div className="footer__mini__company">
-                <div className="footer__mini__company__wrapper">
-                    <a className="footer__mini__company__logo" href="/es" target="_self"></a>
-                    <a className="footer__mini__company__link"
-                       target="_blank"
-                       href="https://rd.rakuten.co.jp/s2/?R2=https%3A%2F%2Fglobal.rakuten.com%2Fcorp%2Fabout%2Fes%2F&amp;D2=638.0.254405.0.32266630&amp;C3=7406e2fdf8a0c2952abce3c27575c64205b95818"
-                       rel="external">Acerca de Rakuten</a>
-                    <a className="footer__mini__company__link" target="_blank" href="/es/pages/impressum">Quiénes
-                        somos</a>
-                    <a className="footer__mini__company__link" target="_blank"
-                       href="/es/pages/terms_and_conditions_of_use">Condiciones
-                        de uso</a>
-                    <a className="footer__mini__company__link" target="_blank"
-                       href="/es/pages/privacy_policy">Política
-                        de privacidad y cookies</a>
-                    <a className="footer__mini__company__link" target="_blank"
-                       href="https://jobs.rakuten.tv" rel="external">Trabaja con
-                        nosotros</a>
+        <div className="mini-footer">
+            <div className="bottom-section">
+                <div className="first-line">
+                    <a className="logo-footer" href=""></a>
+                    <a className="buttons" href="https://rd.rakuten.co.jp/s2/?R2=https%3A%2F%2Fglobal.rakuten.com%2Fcorp%2Fabout%2Fes%2F&D2=638.0.254405.0.32266630&C3=7406e2fdf8a0c2952abce3c27575c64205b95818">acerca de rakuten</a>
+                    <a className="buttons" href="/es/pages/impressum">quienes somos</a>
+                    <a className="buttons" href="/es/pages/terms_and_conditions_of_use">condiciones de uso</a>
+                    <a className="buttons" href="/es/pages/privacy_policy">política de privacidad y cookies</a>
+                    <a className="buttons" href="https://jobs.rakuten.tv">trabaja con nosotros</a>
                 </div>
-                <span
-                    className="footer__mini__company__link footer__mini__company__link--full footer__mini__company__link--nondecored">2009 -
-                    2019 © Rakuten TV Europe, S.L.U. - v1.768.0</span></div>
+                <p className="buttons copyright">2009 - 2019 © Rakuten TV Europe, S.L.U. - v1.776.0</p>
+            </div>
         </div>
     </div>
 
-)
+
+    /*<div className="footer">
+            <div className="footer__info">
+                <h1 className="footer__info__title">Rakuten TV - Tu cine en casa</h1>
+                <p className="footer__info__text">
+                    Disfruta de los últimos estrenos en tu SMART TV y muchos otros
+                    dispositivos en tan
+                    sólo unos clics. Los mejores blockbusters, clásicos premiados y series al instante. Sin contratos ni
+                    compromisos. Pruébalo ahora y gana Rakuten Súper Puntos con cada compra que realices.
+                </p>
+                <a className="footer__info__block footer__info__block--howitworks" href="https://ayuda.rakuten.tv"
+                   target="_blank">
+                    <span className="footer__info__block__title">Cómo funciona</span>
+                    <span className="footer__info__block__text">Rakuten TV es compatible con la gran mayoría de dispositivos conectados.</span>
+                </a>
+                <a className="footer__info__block footer__info__block--payments" href="https://ayuda.rakuten.tv"
+                   target="_blank">
+                    <span className="footer__info__block__title">Más formas de pago</span>
+                    <span className="footer__info__block__text">Rakuten TV es compatible con PayPal y con tarjetas de crédito Visa y Mastercard.</span>
+                </a>
+                <a className="footer__info__block footer__info__block--support" href="https://ayuda.rakuten.tv"
+                   target="_blank">
+                    <span className="footer__info__block__title">Ayuda y soporte</span>
+                    <span className="footer__info__block__text">¿Tienes problemas con nuestro servicio? Ponte en contacto con nuestro equipo de soporte</span>
+                </a>
+            </div>
+            <div className="footer__mini">
+                <div className="footer__mini__company">
+                    <div className="footer__mini__company__wrapper">
+                        <a className="footer__mini__company__logo" href="/es" target="_self"></a>
+                        <a className="footer__mini__company__link"
+                           target="_blank"
+                           href="https://rd.rakuten.co.jp/s2/?R2=https%3A%2F%2Fglobal.rakuten.com%2Fcorp%2Fabout%2Fes%2F&amp;D2=638.0.254405.0.32266630&amp;C3=7406e2fdf8a0c2952abce3c27575c64205b95818"
+                           rel="external">Acerca de Rakuten</a>
+                        <a className="footer__mini__company__link" target="_blank" href="/es/pages/impressum">Quiénes
+                            somos</a>
+                        <a className="footer__mini__company__link" target="_blank"
+                           href="/es/pages/terms_and_conditions_of_use">Condiciones
+                            de uso</a>
+                        <a className="footer__mini__company__link" target="_blank"
+                           href="/es/pages/privacy_policy">Política
+                            de privacidad y cookies</a>
+                        <a className="footer__mini__company__link" target="_blank"
+                           href="https://jobs.rakuten.tv" rel="external">Trabaja con
+                            nosotros</a>
+                    </div>
+                    <span
+                        className="footer__mini__company__link footer__mini__company__link--full footer__mini__company__link--nondecored">2009 -
+                        2019 © Rakuten TV Europe, S.L.U. - v1.768.0</span></div>
+            </div>
+        </div>*/
+
+
+);
 
 export default Footer;
